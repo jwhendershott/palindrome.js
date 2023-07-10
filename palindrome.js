3a4912d9d23e234c61f6fc36962491971419ad92
jwhendershott <jwhendershott@gmail.com>
2023-07-09 23:55:29 -0500

    init commit

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+/* 
+set variables 
+create prompt
+push input from string to array of letters
+reverse array and assign to variable
+test variables against each other
+*/
+
+let input = ""
+let reversedInput = ""
+
+prompt ("Gimme summa dat text...")
+
+
+//test variables again each other
+function isPalindrome() {
+    
+}
